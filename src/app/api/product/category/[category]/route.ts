90ea40c349949891706b0feabeb756ff6be9a952
matiusdimas <dimsmatius@gmail.com>
2023-08-06 23:09:04 +0700

    TESTING

--- src/app/api/product/category/[category]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest, { params }: { params: { category: string } }) {
-    const { category } = params
-    return NextResponse.json({ category })
-}
